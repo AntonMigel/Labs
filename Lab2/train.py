c607c6091addb80d909c7c2231182bafa55d0146
AntonMigel <61519779+AntonMigel@users.noreply.github.com>
2020-04-14 17:35:19 +0300

    Update train.py

--- Lab2/train.py
+++ Lab2/train.py
@@ -95,13 +95,15 @@ def build_model():
         tf.keras.layers.MaxPool2D(),
         tf.keras.layers.Conv2D(filters=8, kernel_size=5),
         tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(filters=8, kernel_size=7),
+        tf.keras.layers.MaxPool2D(),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)
         # Вариант 3
         #tf.keras.layers.Input(shape=(224,224,3)),
-        #tf.keras.layers.Conv2D(filters=8, kernel_size=5),
+        #tf.keras.layers.Conv2D(filters=8, kernel_size=2),
         #tf.keras.layers.MaxPool2D(),
-        #tf.keras.layers.Conv2D(filters=8, kernel_size=5),
+        #tf.keras.layers.Conv2D(filters=8, kernel_size=3),
         #tf.keras.layers.MaxPool2D(),
         #tf.keras.layers.Conv2D(filters=8, kernel_size=5),
         #tf.keras.layers.MaxPool2D(),
